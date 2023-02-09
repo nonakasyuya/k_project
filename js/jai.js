fd89a131a214b9f88e4917aa80790b57acb37a2a
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2023-02-09 18:25:39 +0900

    Create jai.js

---
+++ js/jai.js
@@ -0,0 +1,9 @@
+function jaiphoto(){
+ document.getElementById('jaiphoto').src=
+ './image/jaiphoto.png';
+}
+
+function jaistetas(){
+ document.getElementById('jaiphoto').src=
+'./image/jaistetas.png'; 
+}
