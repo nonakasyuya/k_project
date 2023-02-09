44bfa0e114c2b2d25cdc9423bca25c043ed35cc0
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2023-02-09 17:36:48 +0900

    Create masukekaitou.js

---
+++ js/masukekaitou.js
@@ -0,0 +1,11 @@
+function masukekaitou(){
+ if(document.getElementById('masukeanswer').value=='6'){
+  alert('正解！');
+ }
+  else if(document.getElementById('masukeanswer').value=='６'){
+  alert('正解！');
+}
+  else{
+    alert('不正解答えを確認しよう。');
+ }
+}
