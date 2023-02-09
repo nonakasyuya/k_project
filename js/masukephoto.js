32fde642d39600d8bc3cd1ca5057fe34e7d4cd6e
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2023-02-09 17:35:50 +0900

    Create masukephoto.js

---
+++ js/masukephoto.js
@@ -0,0 +1,10 @@
+function masukephoto(){
+ document.getElementById('masukephoto').src=
+ './image/masukekaitou.png';
+  setTimeout("masukemodosi()",10000);
+}
+
+function masukemodosi(){
+ document.getElementById('masukephoto').src=
+'./image/masukephoto.png'; 
+}
