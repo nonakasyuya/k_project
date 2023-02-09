c87389691c263205a0b62ad7b863b3e88b5dec29
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2023-02-09 17:08:52 +0900

    Create baruphoto.js

---
+++ js/baruphoto.js
@@ -0,0 +1,10 @@
+function baruphoto(){
+ document.getElementById('baruphoto').src=
+ './image/barukaitou.png';
+  setTimeout("barumodosi()",10000);
+}
+
+function barumodosi(){
+ document.getElementById('baruphoto').src=
+'./image/baruphoto.png'; 
+}
