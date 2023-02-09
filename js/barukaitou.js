9575c6055910decce57f82c41b90bd2b13c4fb99
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2023-02-09 17:07:49 +0900

    Create barukaitou.js

---
+++ js/barukaitou.js
@@ -0,0 +1,11 @@
+function sukekaitou(){
+ if(document.getElementById('baruanswer').value=='1908'){
+  alert('正解！');
+ }
+  else if(document.getElementById('baruanswer').value=='１９０８'){
+  alert('正解！');
+}
+  else{
+    alert('不正解答えを確認しよう。');
+ }
+}
