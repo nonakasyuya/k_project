e82f012d1cfb517170434ef0c96fa96731f541d0
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2023-02-09 15:58:57 +0900

    Create sukekaitou.js

---
+++ js/sukekaitou.js
@@ -0,0 +1,11 @@
+function sukekaitou(){
+ if(document.getElementById('sukeanswer').value=='81'){
+  alert('正解！');
+ }
+  else if(document.getElementById('sukeanswer').value=='８１'{
+  alert('正解！');
+}
+  else{
+    alert('不正解答えを確認しよう。');
+ }
+}
