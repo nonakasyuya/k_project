dd45aff3e2c11f3bce8ef2eafce9aecee327348d
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2023-02-10 16:12:34 +0900

    Create aisatu.js

---
+++ js/aisatu.js
@@ -0,0 +1,13 @@
+function finish(){
+setTimeout("owarinoaisatu()",500);
+}
+
+function owarinoaisatu(){
+var str="最後までご覧いただき、ありがとうございました";
+var cnt=document.owari.saigo.value.length;
+if(cnt<22){
+document.owari.saigo.value=str.substr(0,cnt+1);}
+else{
+document.owari.saigo.value="最後までご覧いただき、ありがとうございました";
+}
+}
