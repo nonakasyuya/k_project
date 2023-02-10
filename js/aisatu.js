bbb623c48875b5a7e87bdf8481b8f3b6e7a17c8e
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2023-02-10 16:33:17 +0900

    Update aisatu.js

--- js/aisatu.js
+++ js/aisatu.js
@@ -2,7 +2,9 @@ function owarinoaisatu(){
 var str="最後までご覧いただき、ありがとうございました";
 var cnt=document.owari.saigo.value.length;
 if(cnt<22){
-document.owari.saigo.value=str.substr(0,cnt+1);}
+document.owari.saigo.value=str.substr(0,cnt+1);
+setInterval("owarinoaisatu()",500);
+}
 else{
 document.owari.saigo.value="最後までご覧いただき、ありがとうございました";
 }
