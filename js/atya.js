764c2a89c76b928729ae6efade76d94113d532f9
nonakasyuya <103973985+nonakasyuya@users.noreply.github.com>
2023-02-10 15:31:55 +0900

    Create atya.js

---
+++ js/atya.js
@@ -0,0 +1,9 @@
+function atyaphoto(){
+ document.getElementById('atyaphoto').src=
+ './image/atyaphoto.png';
+}
+
+function atyastetas(){
+ document.getElementById('atyaphoto').src=
+'./image/atyastetas.png'; 
+}
